5d365d3cc71a4d0b2158d1c8e3870f4407d2a141
Yug Mathur <yug.m0578@gmail.com>
2026-08-10 21:28:51 +0530

    Time: 0 ms (100%), Space: 8.5 MB (84.36%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -3,17 +3,14 @@ public:
     int reverse(int x) {
         long long rev = 0;
 
-        long long n = x;
-        if (n < 0)
-            n *= (-1);
-        while (n > 0) {
-            long long digit = n % 10;
+       
+  
+        while (x != 0) {
+            long long digit = x % 10;
             rev = rev * 10 + digit;
-            n /= 10;
+            x /= 10;
         }
-        if (x < 0) {
-            rev *= (-1);
-        }
+ 
         if (rev > INT_MAX || rev < INT_MIN)
             return 0;
         return rev;
