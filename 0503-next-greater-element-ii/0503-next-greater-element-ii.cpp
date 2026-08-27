f862b313165989684d0a08f8b22be8247a27ef7c
Yug Mathur <yug.m0578@gmail.com>
2026-08-27 09:08:43 +0530

    Time: 107 ms (5.49%), Space: 27.9 MB (95.71%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         vector<int> ans(n,-1);
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < (i + n); j++) {
+            for (int j = i + 1; j <= (i + n-1); j++) {
                 int idx = j % n;
                 if (nums[idx] > nums[i]) {
                     ans[i] = nums[idx];
