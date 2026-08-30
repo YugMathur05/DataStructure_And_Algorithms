d97c5874b4ca63bcc3f968cafce05fb1362f07ac
Yug Mathur <yug.m0578@gmail.com>
2026-08-30 13:03:32 +0530

    Time: 0 ms (100%), Space: 23.9 MB (41.33%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,16 +2,34 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
+        int ele_1 = INT_MIN, ele_2 = INT_MIN;
+        int counter_1 = 0, counter_2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (counter_1 == 0 && nums[i] != ele_2) {
+                counter_1++;
+                ele_1 = nums[i];
+            } else if (counter_2 == 0 && nums[i] != ele_1) {
+                counter_2++;
+                ele_2 = nums[i];
+            } else if (nums[i] == ele_1) {
+                counter_1++;
+            } else if (nums[i] == ele_2) {
+                counter_2++;
+            } else {
+                counter_1--;
+                counter_2--;
+            }
+        }
+
+         counter_1 = 0 , counter_2 = 0;
+        for(int i = 0 ; i < n ;i++)
+        {
+            if(nums[i]==ele_1)counter_1++;
+            else if(nums[i]==ele_2)counter_2++;
+        }
         vector<int>ans;
-        unordered_map<int,int>hasharr;
-        for(int i = 0 ; i < n ; i++)
-        {
-            hasharr[nums[i]]++;
-        }
-        for(auto x : hasharr)
-        {
-            if(x.second>n/3)ans.push_back(x.first);
-        }
+        if(counter_1>(n/3))ans.push_back(ele_1);
+        if(counter_2>(n/3))ans.push_back(ele_2);
         return ans;
     }
 };
