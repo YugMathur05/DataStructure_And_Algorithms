10c79f0a0474c8769eedad95664205b2c355a449
Yug Mathur <yug.m0578@gmail.com>
2026-09-22 21:57:16 +0530

    Time: 22 ms (1.12%), Space: 41.8 MB (6.13%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    bool check(char s, char stTop) {
-        if (stTop == '{' && s == '}' || stTop == '(' && s == ')' ||
-            stTop == '[' && s == ']')
+    bool check(char s , stack<char>st) {
+        if (s == ')' && st.top() == '(' || s == '}' && st.top() == '{' ||
+            s == ']' && st.top() == '[')
             return true;
         return false;
     }
@@ -12,14 +12,17 @@ public:
         for (int i = 0; i < n; i++) {
             if (s[i] == '(' || s[i] == '[' || s[i] == '{')
                 st.push(s[i]);
-            else if (!st.empty() && check(s[i], st.top()) == false)
-                return false;
-            else if (!st.empty() && (s[i] == '}' || s[i] == ']' || s[i] == ')'))
-                st.pop();
             else {
-                return false;
+                if (st.empty())
+                    return false;
+                else if (check(s[i],st) == true) {
+                    st.pop();
+                } else {
+                    return false;
+                }
             }
         }
+
         if (st.empty())
             return true;
         return false;
